0d2195c6e569b5e6b706c46035e71e5f24bdae23
Wilfried Tcheumaha <wilfriedtcheumaha@Wilfrieds-Mac-mini.local>
2026-01-19 21:04:21 -0500

    repurposed the initial chat to rag-based chat on amazon products - fast api backend and streamlit front end included

--- apps/api/src/api/app.py
+++ apps/api/src/api/app.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
+from api.api.middleware import RequestIDMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
-from openai import OpenAI
-from groq import Groq
-from google import genai
+from api.api.endpoints import (api_router)
 
-from api.core.config import config
 
 import logging
 
@@ -17,51 +16,17 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-def run_llm(provider, model_name, messages, max_tokens=500):
-    if provider == "OpenAI":
-        client = OpenAI(api_key=config.openai_api_key)
-    elif provider == "Groq":
-        client = Groq(api_key=config.groq_api_key)
-    else:
-        client = genai.Client(api_key=config.google_api_key)
-
-    if provider == "Google":
-        return client.models.generate_content(
-            model=model_name,
-            contents=[message["content"] for message in messages]
-            ).text
-  
-    elif provider == "Groq":
-        return client.chat.completions.create(
-            model=model_name,
-            messages=messages,
-            max_completion_tokens=max_tokens
-            ).choices[0].message.content
-    else:
-        return client.chat.completions.create(
-            model=model_name,
-            messages=messages,
-            max_completion_tokens=max_tokens,
-            reasoning_effort="minimal"
-            ).choices[0].message.content
-
-class ChatRequest(BaseModel):
-    provider: str
-    models_name: str
-    messages: list[dict]
-
-class ChatResponse(BaseModel):
-    message: str
 
 app =FastAPI()
+app.add_middleware(RequestIDMiddleware)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
-@app.post("/chat")
-def chat(
-    request: Request,
-    payload:ChatRequest
-) -> ChatResponse:
-    results= run_llm(payload.provider, payload.models_name, payload.messages)
-
-    return ChatResponse(message=results)
+app.include_router(api_router)
 
 
--- apps/chatbot_ui/src/chatbot_ui/app.py
+++ apps/chatbot_ui/src/chatbot_ui/app.py
@@ -35,18 +35,6 @@ def api_call(method, url, **kwargs):
         return False, {"message": str(e)}
 
 
-with st.sidebar:
-    st.title("Settings")
-    provider = st.selectbox("Provider", ["OpenAI", "Groq", "Google"])
-    if provider == "OpenAI":
-        model_name = st.selectbox("Model", ["gpt-5-nano", "gpt-5-mini"])
-    elif provider == "Groq":
-        model_name = st.selectbox("Model", ["llama-3.3-70b-versatile"])
-    elif provider == "Google":
-        model_name = st.selectbox("Model", ["gemini-2.5-flash"])
-   
-    st.session_state.provider = provider
-    st.session_state.model_name = model_name
 
 if "messages" not in st.session_state:
     st.session_state.messages = [{"role": "assistant", "content": "Hello! How can I help you today?"}]
@@ -61,8 +49,8 @@ if prompt := st.chat_input("Hello how can I assist you today?"):
         st.markdown(prompt)
     
     with st.chat_message("assistant"):
-        output = api_call("post",f"{config.API_URL}/chat", json={"provider": st.session_state.provider, "models_name": st.session_state.model_name, "messages": st.session_state.messages})
+        output = api_call("post",f"{config.API_URL}/rag", json={"query": prompt})
         response_data=output[1]
-        answer=response_data["message"]
+        answer=response_data["answer"]
         st.write(answer)
         st.session_state.messages.append({"role": "assistant", "content": answer})
