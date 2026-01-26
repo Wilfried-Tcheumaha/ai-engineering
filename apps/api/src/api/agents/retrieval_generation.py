43766ea88ae262513a8b6f6233b1695657552a8a
Wilfried Tcheumaha <wilfriedtcheumaha@Wilfrieds-Mac-mini.local>
2026-01-26 03:05:20 -0500

    implement hybrid search

--- apps/api/src/api/agents/retrieval_generation.py
+++ apps/api/src/api/agents/retrieval_generation.py
@@ -41,8 +41,23 @@ def get_embedding(text, model="text-embedding-3-small"):
 def retrieve_data(query, qdrant_client,k=5):
     query_embedding=get_embedding(query)
     results=qdrant_client.query_points(
-        collection_name="Amazon-items-collection-00",
-        query=query_embedding,
+        collection_name="Amazon-items-collection-01-hybrid-search",
+        prefetch=[
+            Prefetch(
+                query=query_embedding,
+                using="text-embedding-3-small",
+                limit=20
+            ),
+            Prefetch(
+                query=Document(
+                    text=query,
+                    model="qdrant/bm25"
+                ),
+                using="bm25",
+                limit=20
+            )
+        ],
+        query=FusionQuery(fusion="rrf"),
         limit=k,
     )
     retrieved_context_ids=[]
@@ -155,9 +170,10 @@ def rag_pipeline_wrapper(question, top_k=5):
 
     for item in result.get("references",[]):
         payload=qdrant_client.query_points(
-            collection_name="Amazon-items-collection-00",
+            collection_name="Amazon-items-collection-01-hybrid-search",
             query=dummy_vector,
             limit=1,
+            using="text-embedding-3-small",
             with_payload=True,
             query_filter=Filter(
                 must=[
